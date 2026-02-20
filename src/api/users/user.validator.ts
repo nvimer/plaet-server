dadb70c534598610d9c108840fdd291b46edf22d
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2026-02-19 23:44:23 -0500

    refactor: remove unused exported UserIdParams in user.validator.ts

--- src/api/users/user.validator.ts
+++ src/api/users/user.validator.ts
@@ -40,6 +40,6 @@ export const userSearchSchema = z.object({
   }),
 });
 
-export type UserIdParams = z.infer<typeof userIdSchema>["params"];
+type UserIdParams = z.infer<typeof userIdSchema>["params"];
 export type UpdateUserInput = z.infer<typeof updateUserSchema>["body"];
 export type UserSearchParams = z.infer<typeof userSearchSchema>["query"];
