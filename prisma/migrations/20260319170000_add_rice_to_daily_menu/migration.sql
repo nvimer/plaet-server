b26b266b8ad9cb7c40600df0c23bb65fb2c6dd15
nicommit <nicommit@ngcraftz.com>
2026-03-19 11:58:15 -0500

    chore(db): add missing rice columns migration for daily menu

---
+++ prisma/migrations/20260319170000_add_rice_to_daily_menu/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "daily_menus" ADD COLUMN "rice_category_id" INTEGER;
+ALTER TABLE "daily_menus" ADD COLUMN "rice_option_1_id" INTEGER;
+ALTER TABLE "daily_menus" ADD COLUMN "rice_option_2_id" INTEGER;
